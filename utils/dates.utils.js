43da906a2258b6fc22975da24ca30b90df9de09e
NelloCarotenuto <nello.carotenuto.96@live.com>
2019-05-06 23:01:28 +0200

    Utils: define a conversion map for months literals

---
+++ utils/dates.utils.js
@@ -0,0 +1,17 @@
+// Define a map from strings to 0-indexed months values
+const months = {
+    'gen' : 0,
+    'feb' : 1,
+    'mar' : 2,
+    'apr' : 3,
+    'mag' : 4,
+    'giu' : 5,
+    'lug' : 6,
+    'ago' : 7,
+    'set' : 8,
+    'ott' : 9,
+    'nov' : 10,
+    'dic' : 11
+};
+
+module.exports.months = months;
