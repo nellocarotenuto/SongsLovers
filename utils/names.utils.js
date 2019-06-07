32285ff951d3c72ae5b6dfe019fee84f3661a9a7
NelloCarotenuto <nello.carotenuto.96@live.com>
2019-06-07 19:48:18 +0200

    Utils: remove another kind of apostrophe when normalizing

--- utils/names.utils.js
+++ utils/names.utils.js
@@ -3,19 +3,22 @@ const diacritics = require('./diacritics.utils');
 
 // Converts unicode chars to ASCII and returns a lowercase string
 function normalize(string) {
-    let normalized = diacritics.remove(string).replace(/[,.'"]/g, '').replace(/[-‐‑⁃−‒–—―]/g, '-').trim().toLowerCase();
-
-    return normalized;
+    return diacritics
+        .remove(string)
+        .replace(/[,.’'"]/g, '')
+        .replace(/[-‐‑⁃−‒–—―]/g, '-')
+        .trim()
+        .toLowerCase();
 }
 
 
 function slugify(text) {
     return normalize(text)
-        .replace(/\s+/g, '-')           // Replace spaces with -
-        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-        .replace(/^-+/, '')             // Trim - from start of text
-        .replace(/-+$/, '');            // Trim - from end of text
+        .replace(/\s+/g, '-')
+        .replace(/[^\w\-]+/g, '')
+        .replace(/\-\-+/g, '-')
+        .replace(/^-+/, '')
+        .replace(/-+$/, '');
 }
 
 // Export the normalize function
