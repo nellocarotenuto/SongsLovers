dcf505bf0d6a6c53bbd156b399a1b6692c8212a9
NelloCarotenuto <nello.carotenuto.96@live.com>
2019-06-22 19:41:03 +0200

    Client: webapp: redirect to home page if no route matches

--- client/webapp/src/app/app-routing.module.ts
+++ client/webapp/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes : Routes = [
-
+  { path : '**', redirectTo : '/' }
 ];
 
 @NgModule({
